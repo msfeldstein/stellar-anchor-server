eb7f9d717985b8955d055fbf1214748f78854d61
Michael Feldstein <msfeldstein@gmail.com>
2019-10-30 16:15:45 -0700

    make new account flow work

--- src/deposit/tasks.py
+++ src/deposit/tasks.py
@@ -23,7 +23,6 @@ logger = logging.getLogger(__name__)
 def create_stellar_deposit(transaction_id):
     """Create and submit the Stellar transaction for the deposit."""
     transaction = Transaction.objects.get(id=transaction_id)
-    print(">>>>> Create stellar deposit")
 
     # We check the Transaction status to avoid double submission of a Stellar
     # transaction. The Transaction can be either `pending_anchor` if the task
@@ -43,86 +42,42 @@ def create_stellar_deposit(transaction_id):
 
     # We can assume transaction has valid stellar_account, amount_in, and asset
     # because this task is only called after those parameters are validated.
-    stellar_account = transaction.stellar_account
     payment_amount = round(transaction.amount_in - transaction.amount_fee, 7)
-    asset = transaction.asset.code
-
+    
     # If the given Stellar account does not exist, create
     # the account with at least enough XLM for the minimum
     # reserve and a trust line (recommended 2.01 XLM), update
     # the transaction in our internal database, and return.
-    print(">>> Stellar account" + stellar_account)
     address = Address(
-        stellar_account,
+        transaction.stellar_account,
         network=settings.STELLAR_NETWORK,
         horizon_uri=settings.HORIZON_URI,
     )
+    # Check to see if the account exists. If so, do a normal payment.
+    # If not, use the claimable payment mechanism
     try:
         address.get()
-        print(">>>> Actually i did get the account")
+        import json
+        print(f">>>> account ${json.dumps(address.balances, indent = 4)}")
+        perform_standard_payment(transaction, payment_amount)
     except HorizonError as address_exc:
-        print(">>>> No address yet")
         # 404 code corresponds to Resource Missing.
         if address_exc.status_code != 404:
+            # If this is not a 404, something unknown went wrong.
             logger.debug(
                 "error with message %s when loading stellar account",
                 address_exc.message,
             )
-            print(">>>> Not 404")
-            return
-        intermediate_account = Keypair.random()
-        intermediate_key = intermediate_account.address().decode()
-        print(">>>> Generated random account " + intermediate_key)
-        starting_balance = settings.ACCOUNT_STARTING_BALANCE
-        anchor_keypair = Keypair.from_seed(settings.STELLAR_DISTRIBUTION_ACCOUNT_SEED)
-        anchor_address = anchor_keypair.address()
-        print(f">>>> Distribution account {anchor_address}")
-        builder = Builder(
-            secret=settings.STELLAR_DISTRIBUTION_ACCOUNT_SEED,
-            horizon_uri=settings.HORIZON_URI,
-            network=settings.STELLAR_NETWORK,
-        )
-        print(">>>> create_account_op")
-        builder.append_create_account_op(
-            destination=intermediate_key,
-            starting_balance="40",
-            source=anchor_address,
-        )
-        print(">>>> change_trust")
-        builder.append_change_trust_op(
-            transaction.asset.code, transaction.asset.issuer,
-            source=intermediate_key)
-        print(">>>> payment")
-        builder.append_payment_op(
-            source=anchor_address,
-            destination=intermediate_key,
-            asset_code=transaction.asset.code,
-            asset_issuer=transaction.asset.issuer,
-            amount=str(payment_amount)
-        )
-        print(">>>> set_options")
-        builder.append_set_options_op(
-            source=intermediate_key,
-            master_weight=0,
-            signer_address=transaction.stellar_account,
-            signer_weight=1,
-            signer_type="ed25519PublicKey",
-        )
-        builder.sign()
-        builder.sign(intermediate_account.seed())
-        try:
-            builder.submit()
-        except HorizonError as submit_exc:
-            print(">>>> Something went wrong!!!!! ")
-            # op_src_no_trust means you don't have any assets or trustlines in the distribution account. Run create-stellar-token
-            print(submit_exc)
-            logger.debug(f"error with message {submit_exc.message} when submitting create account to horizon")
-            return
-        print(">>>>> Submitted intermediate account " + intermediate_key)
-        transaction.status = Transaction.STATUS.pending_user
+            raise address_exc
+        create_claimable_account(transaction, payment_amount)
+        transaction.status = Transaction.STATUS.completed
         transaction.save()
         return
+    
 
+    
+
+def perform_standard_payment(transaction, payment_amount):
     # If the account does exist, deposit the desired amount of the given
     # asset via a Stellar payment. If that payment succeeds, we update the
     # transaction to completed at the current time. If it fails due to a
@@ -134,9 +89,9 @@ def create_stellar_deposit(transaction_id):
         network=settings.STELLAR_NETWORK,
     )
     builder.append_payment_op(
-        destination=stellar_account,
-        asset_code=asset,
-        asset_issuer=settings.STELLAR_ISSUER_ACCOUNT_ADDRESS,
+        destination=transaction.stellar_account,
+        asset_code=transaction.asset.code,
+        asset_issuer=transaction.asset.issuer,
         amount=str(payment_amount),
     )
     builder.sign()
@@ -168,6 +123,56 @@ def create_stellar_deposit(transaction_id):
     transaction.amount_out = payment_amount
     transaction.save()
 
+def create_claimable_account(transaction, payment_amount):
+    intermediate_account = Keypair.random()
+    intermediate_key = intermediate_account.address().decode()
+    print(">>>> Generated random account " + intermediate_key)
+    starting_balance = settings.ACCOUNT_STARTING_BALANCE
+    anchor_keypair = Keypair.from_seed(settings.STELLAR_DISTRIBUTION_ACCOUNT_SEED)
+    anchor_address = anchor_keypair.address()
+    print(f">>>> Distribution account {anchor_address}")
+    builder = Builder(
+        secret=settings.STELLAR_DISTRIBUTION_ACCOUNT_SEED,
+        horizon_uri=settings.HORIZON_URI,
+        network=settings.STELLAR_NETWORK,
+    )
+    print(">>>> create_account_op")
+    builder.append_create_account_op(
+        destination=intermediate_key,
+        starting_balance="40",
+        source=anchor_address,
+    )
+    print(">>>> change_trust")
+    builder.append_change_trust_op(
+        transaction.asset.code, transaction.asset.issuer,
+        source=intermediate_key)
+    print(">>>> payment")
+    builder.append_payment_op(
+        source=anchor_address,
+        destination=intermediate_key,
+        asset_code=transaction.asset.code,
+        asset_issuer=transaction.asset.issuer,
+        amount=str(payment_amount)
+    )
+    print(">>>> set_options")
+    builder.append_set_options_op(
+        source=intermediate_key,
+        master_weight=0,
+        signer_address=transaction.stellar_account,
+        signer_weight=1,
+        signer_type="ed25519PublicKey",
+    )
+    builder.sign()
+    builder.sign(intermediate_account.seed())
+    try:
+        builder.submit()
+    except HorizonError as submit_exc:
+        print(">>>> Something went wrong!!!!! ")
+        # op_src_no_trust means you don't have any assets or trustlines in the distribution account. Run create-stellar-token
+        print(submit_exc)
+        logger.debug(f"error with message {submit_exc.message} when submitting create account to horizon")
+        return
+    print(">>>>> Submitted intermediate account " + intermediate_key)
 
 @periodic_task(run_every=(crontab(minute="*/1")), ignore_result=True)
 def check_trustlines():
@@ -175,7 +180,6 @@ def check_trustlines():
     Create Stellar transaction for deposit transactions marked as pending trust, if a
     trustline has been created.
     """
-    print("HELLO I AM RUNNING check_trustlines")
     transactions = Transaction.objects.filter(
         status=Transaction.STATUS.pending_trust)
     horizon = Horizon(horizon_uri=settings.HORIZON_URI)
